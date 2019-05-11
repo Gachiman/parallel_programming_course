578c17727ffe56a360a6149efd517f7ee4f92177
Ilya Aglikov <aglikov@bk.ru>
2019-05-11 20:22:17 +0300

    V1.2

--- modules/task_3/aglikov_i_shellsort_divide_et_impera_tbb/main.cpp
+++ modules/task_3/aglikov_i_shellsort_divide_et_impera_tbb/main.cpp
@@ -1,38 +1,49 @@
 //  Copyright 2019 Aglikov Ilya
 
 #define _SCL_SECURE_NO_WARNINGS
-#include <tbb/tbb.h>
+#include <omp.h>
 #include <iostream>
 #include <algorithm>
 #include <utility>
+#include <ctime>
+
 
 void shellsort(int* arr, int size);
+//void shellsortPar(int* arr, int size);
 void shellsortPar(int* arr, int size, int procNum);
 void printArr(int* arr, int size);
+bool checkEquality(int* arr0, int* arr1, int size);
+void mergeArray(int* arr, int size, int procNum);
 void merge(int* arr, int n, int m);
 
 int main(int argc, char* argv[]) {
     int size = ((argc >= 2) && (atoi(argv[1]) > 0)) ? atoi(argv[1]) : 10;
     int procNum = ((argc >= 3) && (atoi(argv[2]) > 0)) ?
-        atoi(argv[2]) : tbb::task_scheduler_init::default_num_threads();
-    tbb::task_scheduler_init init(procNum);
+        atoi(argv[2]) : omp_get_max_threads();
+    omp_set_num_threads(procNum);
 
     int* arrSingle = new int[size];
     int* arrPar = new int[size];
+    std::srand(static_cast<unsigned int>(time(0)));
     for (int i = 0; i < size; i++)
-        arrSingle[i] = arrPar[i] = size - i;
+        arrSingle[i] = arrPar[i] = std::rand() % 100;
     if (size <= 30) {
         std::cout << "Original array: ";
         printArr(arrSingle, size);
     }
 
-    tbb::tick_count shellStart = tbb::tick_count::now();
+    double shellTime = omp_get_wtime();
     shellsort(arrSingle, size);
-    tbb::tick_count shellEnd = tbb::tick_count::now();
+    shellTime = omp_get_wtime() - shellTime;
 
-    tbb::tick_count parStart = tbb::tick_count::now();
-    shellsortPar(arrPar, size, procNum);
-    tbb::tick_count parEnd = tbb::tick_count::now();
+    double parTime = omp_get_wtime();
+    //shellsortPar(arrPar, size);
+#pragma omp parallel shared(arrPar)
+    {
+#pragma omp single nowait
+        shellsortPar(arrPar, size, procNum);
+    }
+    parTime = omp_get_wtime() - parTime;
 
     if (size <= 30) {
         std::cout << "Shell sort array: ";
@@ -40,19 +51,14 @@ int main(int argc, char* argv[]) {
         std::cout << std::endl;
     }
 
-    std::cout << std::fixed << "Single time = " << (shellEnd - shellStart).seconds()
-        << "\nParallel time = " << (parEnd - parStart).seconds()
-        << "\nAcceleration = "
-        << ((shellEnd - shellStart).seconds() / (parEnd - parStart).seconds())
-        << std::endl;
+    std::cout << std::fixed << "Single time = " << shellTime
+        << "\nParallel time = " << parTime
+        << "\nAcceleration = " << (shellTime / parTime) << std::endl;
 
-    if (std::equal(arrSingle, arrSingle + size, arrPar))
+    if (checkEquality(arrSingle, arrPar, size))
         std::cout << "Sort working right.\n";
     else
         std::cout << "Sort working wrong.\n";
-
-    delete[] arrSingle;
-    delete[] arrPar;
     return 0;
 }
 
@@ -70,19 +76,47 @@ void shellsort(int* arr, int size) {
     }
 }
 
+/*void shellsortPar(int* arr, int size) {
+    int procNum = omp_get_max_threads();
+    if ((procNum == 1) || (size <= procNum * 2)) {
+        shellsort(arr, size);
+    }  else {
+#pragma omp parallel
+        {
+            int threadNum = omp_get_thread_num();
+            if (threadNum != procNum - 1)
+                shellsort(arr + (size / procNum) * threadNum, size / procNum);
+            else
+                shellsort(arr + (size / procNum) * threadNum,
+                    size - (size / procNum) * threadNum);
+        }
+        mergeArray(arr, size, procNum);
+    }
+}*/
+
 void shellsortPar(int* arr, int size, int procNum) {
     if ((procNum == 1) || (size < procNum * 2)) {
         shellsort(arr, size);
-    } else {
-        tbb::task_group tg;
-        tg.run([&arr, size, procNum] {
-            shellsortPar(arr, size / 2, procNum / 2);
-        });
-        tg.run([&arr, size, procNum] {
-            shellsortPar(arr + size / 2, size - size / 2, procNum - procNum / 2);
-        });
-        tg.wait();
+    }
+    else {
+#pragma omp task firstprivate(procNum) shared(arr)
+        shellsortPar(arr, size / 2, procNum / 2);
+#pragma omp task firstprivate(procNum) shared(arr)
+        shellsortPar(arr + size / 2, size - size / 2, procNum - procNum / 2);
+#pragma omp taskwait
         merge(arr, size / 2, size - size / 2);
+    }
+}
+
+void mergeArray(int* arr, int size, int procNum) {
+    int count = 1, div = procNum;
+    while (procNum > 1) {
+        if (procNum == 2)
+            merge(arr, size / div * count, size - size / div * count);
+        else
+            merge(arr, size / div * count, size / div);
+        count++;
+        procNum--;
     }
 }
 
@@ -102,13 +136,20 @@ void merge(int* arr, int n, int m) {
         arr[k++] = leftArr[i++];
     while (j < m)
         arr[k++] = rightArr[j++];
-
-    delete[] leftArr;
-    delete[] rightArr;
 }
 
 void printArr(int* arr, int size) {
     for (int i = 0; i < size; i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
+}
+
+bool checkEquality(int* arr0, int* arr1, int size) {
+    bool flag = true;
+    for (int i = 0; i < size; i++)
+        if (arr0[i] != arr1[i]) {
+            flag = false;
+            break;
+        }
+    return flag;
 }
