864f20dd887bad5fe52dd5b2ffde0443738a8d05
Ilya Aglikov <aglikov@bk.ru>
2019-05-11 20:55:33 +0300

    V1.2

--- modules/task_2/aglikov_i_shellsort_divide_et_impera_omp/main.cpp
+++ modules/task_2/aglikov_i_shellsort_divide_et_impera_omp/main.cpp
@@ -5,23 +5,26 @@
 #include <iostream>
 #include <algorithm>
 #include <utility>
+#include <ctime>
+
 
 void shellsort(int* arr, int size);
-void shellsortPar(int* arr, int size);
+void shellsortPar(int* arr, int size, int procNum);
 void printArr(int* arr, int size);
 bool checkEquality(int* arr0, int* arr1, int size);
-void mergeArray(int* arr, int size, int procNum);
 void merge(int* arr, int n, int m);
 
 int main(int argc, char* argv[]) {
     int size = ((argc >= 2) && (atoi(argv[1]) > 0)) ? atoi(argv[1]) : 10;
-    if ((argc == 3) && (atoi(argv[2])))
-        omp_set_num_threads(atoi(argv[2]));
+    int procNum = ((argc >= 3) && (atoi(argv[2]) > 0)) ?
+        atoi(argv[2]) : omp_get_max_threads();
+    omp_set_num_threads(procNum);
 
     int* arrSingle = new int[size];
     int* arrPar = new int[size];
+    std::srand(static_cast<unsigned int>(time(0)));
     for (int i = 0; i < size; i++)
-        arrSingle[i] = arrPar[i] = size - i;
+        arrSingle[i] = arrPar[i] = std::rand() % 100;
     if (size <= 30) {
         std::cout << "Original array: ";
         printArr(arrSingle, size);
@@ -32,7 +35,11 @@ int main(int argc, char* argv[]) {
     shellTime = omp_get_wtime() - shellTime;
 
     double parTime = omp_get_wtime();
-    shellsortPar(arrPar, size);
+#pragma omp parallel shared(arrPar)
+    {
+#pragma omp single nowait
+        shellsortPar(arrPar, size, procNum);
+    }
     parTime = omp_get_wtime() - parTime;
 
     if (size <= 30) {
@@ -49,6 +56,8 @@ int main(int argc, char* argv[]) {
         std::cout << "Sort working right.\n";
     else
         std::cout << "Sort working wrong.\n";
+    delete[] arrSingle;
+    delete[] arrPar;
     return 0;
 }
 
@@ -66,33 +75,16 @@ void shellsort(int* arr, int size) {
     }
 }
 
-void shellsortPar(int* arr, int size) {
-    int procNum = omp_get_max_threads();
-    if ((procNum == 1) || (size <= procNum * 2)) {
+void shellsortPar(int* arr, int size, int procNum) {
+    if ((procNum == 1) || (size < procNum * 2)) {
         shellsort(arr, size);
-    }  else {
-#pragma omp parallel
-        {
-            int threadNum = omp_get_thread_num();
-            if (threadNum != procNum - 1)
-                shellsort(arr + (size / procNum) * threadNum, size / procNum);
-            else
-                shellsort(arr + (size / procNum) * threadNum,
-                    size - (size / procNum) * threadNum);
-        }
-        mergeArray(arr, size, procNum);
-    }
-}
-
-void mergeArray(int* arr, int size, int procNum) {
-    int count = 1, div = procNum;
-    while (procNum > 1) {
-        if (procNum == 2)
-            merge(arr, size / div * count, size - size / div * count);
-        else
-            merge(arr, size / div * count, size / div);
-        count++;
-        procNum--;
+    } else {
+#pragma omp task firstprivate(procNum) shared(arr)
+        shellsortPar(arr, size / 2, procNum / 2);
+#pragma omp task firstprivate(procNum) shared(arr)
+        shellsortPar(arr + size / 2, size - size / 2, procNum - procNum / 2);
+#pragma omp taskwait
+        merge(arr, size / 2, size - size / 2);
     }
 }
 
@@ -112,6 +104,8 @@ void merge(int* arr, int n, int m) {
         arr[k++] = leftArr[i++];
     while (j < m)
         arr[k++] = rightArr[j++];
+    delete[] leftArr;
+    delete[] rightArr;
 }
 
 void printArr(int* arr, int size) {
